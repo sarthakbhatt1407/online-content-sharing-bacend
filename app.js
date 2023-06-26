785389b808a31cf8355cc25f69fd28a502951907
Sarthak Bhatt <sarthakbhatt1407@gmail.comm>
2023-06-26 17:21:26 +0530

    added port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const postsRoute = require("./routes/posts");
 const userRoute = require("./routes/user");
 // const auth = require("./middleware/auth");
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
